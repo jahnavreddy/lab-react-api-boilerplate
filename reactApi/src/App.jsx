3bab9504ecf12ea8f6cfed0b5ea1df4207713491
jahnavreddy <jahnavredy12@gmail.com>
2024-02-29 12:29:01 +0530

    projectDone

---
+++ reactApi/src/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import './App.css'
+import axios from "axios"
+
+function App() {
+
+  const url = "https://newsapi.org/v2/everything?q=india&apiKey=11d5d2d7f1ab4f5f90c53750664e22c0" ;
+
+  const [data,setData] = useState([]);
+
+  
+  useEffect(() => {
+    const fetchData = () => {
+      axios.get("https://reactnd-books-api.udacity.com/books", {
+        headers: { 'Authorization': 'whatever-you-want' }
+      })
+      .then(response => {
+        setData(response.data.books);
+      })
+      .catch(error => {
+        console.log(error)
+        console.log(error.message);
+        console.log("Status Code : ",error.request.status)
+      });
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(data);
+
+  return (
+    <div>
+      <h1>Books</h1>
+      {data.map((book, idx) => (
+        <div className='bookdiv' key={idx}>
+          <h2>{book.title}</h2>
+          <div className='bookdetails'>
+            <img src={book.imageLinks.thumbnail} alt="bookimg" />
+            <p>{book.description}</p>
+          </div>
+          <h3>{book.authors}</h3>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App
